88ae16bd05da3f4fdd6131e7b14e26f6b8fad21c
SEOHONU <shw12050708@gmail.com>
2023-06-05 17:44:32 +0900

    서현우 / 230605 commit
    커뮤니티 DAO, mapper, Controller, Service 첫 생성.
    작성 폼 - <select> 태그 관련 디테일 수정 필요.

--- src/main/java/kh/final_project/controllers/CommunityController.java
+++ src/main/java/kh/final_project/controllers/CommunityController.java
@@ -1,15 +1,34 @@
 package kh.final_project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kh.final_project.services.CommunityService;
 
 @Controller
 @RequestMapping("/community/")
 public class CommunityController {
+	@Autowired
+	private CommunityService communityService;
 
 	@RequestMapping("toNotification")
 	public String toNotification() {
 		System.out.println("toNotification");
 		return "community/notification";
+	}
+
+	@RequestMapping("toWriteForm")
+	public String toWriteForm(String boardType, Model model) {
+		System.out.println("toWriteForm");
+		System.out.println("boardType : " + boardType);
+		model.addAttribute("boardType", boardType);
+		return "community/writeForm";
+	}
+
+	@RequestMapping("insertBoard")
+	public void insertBoard() {
+		System.out.println("insertBoard");
 	}
 }
