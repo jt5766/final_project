4fd855d57c0d8b40165b69ebe0989a35b74396c1
SeokJune <op2se1@gmail.com>
2023-06-07 09:13:14 +0900

    Update CategoryType.java
    TYPE TABLE 1,2 통합 DTO

--- src/main/java/kh/final_project/dto/CategoryType.java
+++ src/main/java/kh/final_project/dto/CategoryType.java
@@ -1,30 +1,57 @@
 package kh.final_project.dto;
 
 public class CategoryType {
-    private Integer code;
-    private String name;
+	private Integer code;
+	private Integer sort;
+	private String name;
+	private String yn;
 
+	public CategoryType() {
+	}
 
-    public CategoryType() {}
+	public CategoryType(Integer code, String name) {
+		this.code = code;
+		this.name = name;
+	}
 
-    public CategoryType(Integer code, String name) {
-        this.code = code;
-        this.name = name;
-    }
+	public CategoryType(Integer code, Integer sort, String name, String yn) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.sort = sort;
+		this.yn = yn;
+	}
 
-    public Integer getCode() {
-        return code;
-    }
+	public Integer getCode() {
+		return code;
+	}
 
-    public void setCode(Integer code) {
-        this.code = code;
-    }
+	public void setCode(Integer code) {
+		this.code = code;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSort() {
+		return sort;
+	}
+
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+
+	public String getYn() {
+		return yn;
+	}
+
+	public void setYn(String yn) {
+		this.yn = yn;
+	}
+
 }
