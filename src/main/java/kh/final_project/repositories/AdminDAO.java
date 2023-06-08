b4fd973c5de4bff80bc34ab62309207f47620df3
SeokJune <op2se1@gmail.com>
2023-06-08 09:12:36 +0900

    del test

--- src/main/java/kh/final_project/repositories/AdminDAO.java
+++ src/main/java/kh/final_project/repositories/AdminDAO.java
@@ -16,12 +16,12 @@ public class AdminDAO {
 	SqlSessionTemplate mybatis;
 
 	public List<CategoryType> selectGenreType(CategoryType cy) {
-		mybatis.selectOne("Admin.Proc_GenreType", cy);
-		System.out.println(cy);
-
-		Map<String, List<CategoryType>> result = new HashMap<String, List<CategoryType>>();
-
-		mybatis.selectOne("Admin.Proc_GenreType_", result);
-		return result.get("result");
+//		mybatis.selectOne("Admin.Proc_GenreType", cy);
+//		System.out.println(cy);
+//
+//		Map<String, List<CategoryType>> result = new HashMap<String, List<CategoryType>>();
+//
+//		mybatis.selectOne("Admin.Proc_GenreType_", result);
+//		return result.get("result");
 	}
 }
